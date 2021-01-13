b6a3e35fb8ccee6f311be8eaac2bee95faf960e7
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-13 17:59:36 +0530

    everything is positioned . Not Responsive

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,19 +3,18 @@ function App() {
   return (
     <div className="App d-flex flex-column">
       <h1 className="mt-3">Your ToDo App</h1>
-      <div className="container mx-0 px-0 w-100 d-flex flex-row">
-        <div className="Todo__Res">
+      <div className="container-fluid mx-0 px-0 ">
+        <div className="Todo__Res mx-5">
           <h2>Your ToDo List</h2>
           <ul className="Todo__List">
             <li>Nothing</li>
           </ul>
         </div>
-        <div className="Todo__Add">
+        <div className="Todo__Add mx-5">
           <h2>Add Your ToDo Here</h2>
           <p>hello</p>
         </div>
       </div>
-      <p>kkkk</p>
     </div>
   );
 }
