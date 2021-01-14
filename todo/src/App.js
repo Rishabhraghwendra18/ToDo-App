bd05c7d7872c70e71129409f12fa21a30dd03c8f
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-14 18:17:37 +0530

    submit function is working perfectly

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,10 @@
 import './styles/App.css';
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap'
 function App() {
+  const [todoList, setTodo] = useState([]);
+  const [Ttitle, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   return (
     <div className="App d-flex flex-column">
       <h1 className="mt-3">Your ToDo App</h1>
@@ -7,12 +12,31 @@ function App() {
         <div className="Todo__Res mx-5">
           <h2>Your ToDo List</h2>
           <ul className="Todo__List">
-            <li>Nothing</li>
+            {todoList ? todoList.map((item, index) => <li key={index}>{item.title}</li>) : 0}
           </ul>
         </div>
         <div className="Todo__Add mx-5">
-          <h2>Add Your ToDo Here</h2>
-          <p>hello</p>
+          <h2>Add Your ToDo</h2>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Take Jack to School..." onChange={e=>setTitle(e.target.value)}/>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Buy him a cake..."onChange={e => setDesc(e.target.value)} onDefault={desc} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={(e) => {
+              e.preventDefault();
+              const kk = {
+                title: Ttitle,
+                desc: desc
+              }
+              setTodo([...todoList, kk]);
+            }}>
+              Submit
+            </Button>
+          </Form>
         </div>
       </div>
     </div>
