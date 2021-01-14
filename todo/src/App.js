cf1d3a0465bdfbcd5cdd1fa368bae3e35937fbe3
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-14 18:46:52 +0530

    dropdown is ready

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import { useState } from 'react';
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap';
+import DropDown from './components/DropDowns';
 function App() {
   const [todoList, setTodo] = useState([]);
   const [Ttitle, setTitle] = useState('');
@@ -11,20 +12,22 @@ function App() {
       <div className="container-fluid mx-0 px-0">
         <div className="Todo__Res mx-5">
           <h2>Your ToDo List</h2>
-          <ul className="Todo__List">
-            {todoList ? todoList.map((item, index) => <li key={index}>{item.title}</li>) : 0}
-          </ul>
+          <div className="List">
+            <ul className="Todo__List">
+              {todoList ? todoList.map((item, index) => <li key={index}>{item.title}<DropDown desc={item.desc}/></li>) : 0}
+            </ul>
+          </div>
         </div>
         <div className="Todo__Add mx-5">
           <h2>Add Your ToDo</h2>
           <Form>
             <Form.Group controlId="exampleForm.ControlInput1">
               <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="Take Jack to School..." onChange={e=>setTitle(e.target.value)}/>
+              <Form.Control type="text" placeholder="Take Jack to School..." onChange={e => setTitle(e.target.value)} />
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
               <Form.Label>Description</Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder="Buy him a cake..."onChange={e => setDesc(e.target.value)} onDefault={desc} />
+              <Form.Control as="textarea" rows={3} placeholder="Buy him a cake..." onChange={e => setDesc(e.target.value)} onDefault={desc} />
             </Form.Group>
             <Button variant="primary" type="submit" onClick={(e) => {
               e.preventDefault();
